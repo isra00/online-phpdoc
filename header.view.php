b444533b2d2af15c29adefde6a208578454722b0
Isra <isra00@gmail.com>
2013-02-02 03:06:54 +0100

    CSS

--- header.view.php
+++ header.view.php
@@ -7,6 +7,8 @@
   .notify { padding: 0 1em; }
   .notify.success { border: 1px solid #be7; background: #f8fff0; }
   .repo-list td { padding: .2em 2em .2em .1em; }
+  
+  .error { color: red; }
   </style>
 </head>
 <body>
