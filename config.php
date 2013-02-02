e38cbfa112607399d33990b32311711efab1a19f
Isra <isra00@gmail.com>
2013-02-02 23:18:22 +0100

    Add GitHub's IPs for web hooks

--- config.php
+++ config.php
@@ -10,6 +10,14 @@ define('GITHUB_CLIENT_SECRET', getenv('GITHUB_CLIENT_SECRET'));
 define('GITHUB_CALLBACK',      WEB_ROOT . 'gh-auth.php');
 define('GITHUB_HOOK_URL',      WEB_ROOT . 'github_webhook.php');
 
+//IP from which web hook must be called
+$GITHUB_HOOK_IP = array(
+    '207.97.227.253',
+    '50.57.128.197',
+    '108.171.174.178',
+    '50.57.231.61'
+);
+
 define('MYSQL_HOST',           getenv('MYSQL_HOST'));
 define('MYSQL_USER',           getenv('MYSQL_USER'));
 define('MYSQL_PASSWORD',       getenv('MYSQL_PASSWORD'));
