844e53cf24ab6d07bb45e89c9ab9a786262e9ae7
Isra <isra00@gmail.com>
2013-02-02 03:07:14 +0100

    Fix init for auth pages

--- init.php
+++ init.php
@@ -1,6 +1,11 @@
 <?php
 
+//Load config
 include 'config.php';
+
+//Load libs
+require 'db.php';
+require 'misc.php';
 
 /**
  * Session handling
@@ -8,22 +13,20 @@ include 'config.php';
 
 @session_start();
 
-if ($_SERVER['SCRIPT_NAME'] != 'gh-login.php') {
-  if (empty($_SESSION['github']['access_token'])) {
+if (basename($_SERVER['SCRIPT_NAME']) != 'gh-login.php' && basename($_SERVER['SCRIPT_NAME']) != 'gh-auth.php')
+{
+  if (empty($_SESSION['github']['access_token']))
+  {
     header('Location: gh-login.php');
     die;
   }
+  
+  //User GitHub data is cached in Session for better performance
+  if (!isset($_SESSION['github']['user_data']))
+  {
+    $_SESSION['github']['user_data'] = github_api('user');
+  }
+
+  //Init DB
+  $db_conn = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
 }
-
-//User GitHub data is cached in Session for better performance
-if (!isset($_SESSION['github']['user_data']))
-{
-  $_SESSION['github']['user_data'] = github_api('user');
-}
-
-//Init DB
-$db_conn = new mysqli(MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE);
-
-//Load libs
-require 'db.php';
-require 'misc.php';
