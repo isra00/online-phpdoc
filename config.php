84d8dc5bcaa63e7150e566584f1da5965fc7ea71
Isra <isra00@gmail.com>
2013-02-02 03:06:40 +0100

    New hardcoded root

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('GITHUB_CLIENT_SECRET', getenv('GITHUB_CLIENT_SECRET'));
 define('GITHUB_CALLBACK', getenv('GITHUB_CALLBACK'));
 
 /** @todo calculate WEB_ROOT automatically */
-define('WEB_ROOT', 'http://localhost/pruebas/cloudphpdoc/');
+define('WEB_ROOT', 'http://localhost/online-phpdoc/');
 define('LOCAL_ROOT', __DIR__ );
 
 define('MYSQL_HOST', getenv('MYSQL_HOST'));
