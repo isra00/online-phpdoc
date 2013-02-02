dc1e33eb9556e10a5abf25d46d2135a7a8cfe354
Isra <isra00@gmail.com>
2013-02-02 01:55:04 +0100

    Fix syntax error

--- db.php
+++ db.php
@@ -59,7 +59,7 @@ function db_update_repo_status($id_job, $status)
   global $db_conn;
   
   $stmt = $db_conn->prepare('UPDATE job SET status = ?, UPDATED = 0 WHERE id_job = ?');
-  $stmt->bind_param('si', $status, $i d_job);
+  $stmt->bind_param('si', $status, $id_job);
   $stmt->execute();
   return $stmt->affected_rows;
 }
