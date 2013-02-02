afc2941486d12193dc69e78ed39d0d0f82f29b2a
Isra <isra00@gmail.com>
2013-02-02 01:52:36 +0100

    New functions for updating repo and job status

--- db.php
+++ db.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * DML functions
+ */
+
 function db_search_repo($service, $location)
 {
   global $db_conn;
@@ -29,4 +33,33 @@ function db_insert_job($id_repo, $changeset)
   $stmt->bind_param('is', $id_repo, $changeset);
   $stmt->execute();
   return $stmt->insert_id;
+}
+
+function db_select_first_job()
+{
+  global $db_conn;
+  
+  $res = $db_conn->query("SELECT * FROM job JOIN repo ON job.id_repo = repo.id_repo WHERE status = 'waiting' ORDER BY job.created ASC LIMIT 1");
+  return $res->fetch_assoc();
+  
+}
+
+function db_update_job_status($id_job, $status)
+{
+  global $db_conn;
+  
+  $stmt = $db_conn->prepare('UPDATE job SET status = ?, UPDATED = 0 WHERE id_job = ?');
+  $stmt->bind_param('si', $status, $i d_job);
+  $stmt->execute();
+  return $stmt->affected_rows;
+}
+
+function db_update_repo_status($id_job, $status)
+{
+  global $db_conn;
+  
+  $stmt = $db_conn->prepare('UPDATE job SET status = ?, UPDATED = 0 WHERE id_job = ?');
+  $stmt->bind_param('si', $status, $i d_job);
+  $stmt->execute();
+  return $stmt->affected_rows;
 }
