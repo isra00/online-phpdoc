2aee46f42bd901029be6e4d81612e70f07b89e5c
Isra <isra00@gmail.com>
2013-02-04 23:50:32 +0100

    Google Analytics JS code

--- config.php
+++ config.php
@@ -22,3 +22,5 @@ define('MYSQL_HOST',           getenv('MYSQL_HOST'));
 define('MYSQL_USER',           getenv('MYSQL_USER'));
 define('MYSQL_PASSWORD',       getenv('MYSQL_PASSWORD'));
 define('MYSQL_DATABASE',       getenv('MYSQL_DATABASE'));
+
+define('GA_ID',                 getenv('GA_ID'));
