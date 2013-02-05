d13ca4327a23ca56b433704d3f5ce8f67388347f
Israel Viana <isra00@gmail.com>
2013-02-05 21:13:15 +0100

    Include HTML header in GitHub log-in page

--- gh-login.php
+++ gh-login.php
@@ -1,3 +1,4 @@
 <?php include 'init.php' ?>
+<?php include 'header.view.php' ?>
 <?php /** @todo usar el parámetro state para mayor seguridad */ ?>
 <a href="https://github.com/login/oauth/authorize?client_id=<?php print GITHUB_CLIENT_ID ?>&scope=public_repo">Log-in with GitHub</a>
