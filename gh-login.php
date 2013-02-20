f08e1467350f8e6a74f02c575ee3331c37677bcb
Isra <isra00@gmail.com>
2013-02-20 23:40:11 +0100

    Minimal landing page with HTML5 boilerplate and Bootstrap CSS

--- gh-login.php
+++ gh-login.php
@@ -1,3 +1,15 @@
 <?php include 'init.php' ?>
 <?php /** @todo usar el parámetro state para mayor seguridad */ ?>
-<a href="https://github.com/login/oauth/authorize?client_id=<?php print GITHUB_CLIENT_ID ?>&scope=public_repo">Log-in with GitHub</a>
+
+<?php include 'header.view.php' ?>
+
+<div class="landing hero-unit">
+  <h1>Your phpDocs in the cloud</h1>
+  <p>Forget about generating and hosting your PHP code documents. <br>
+    Just <strong>one click</strong> and they will be generated automatically from your GitHub repositories.</p>
+  <p>
+    <a class="btn btn-primary btn-large github-login" href="https://github.com/login/oauth/authorize?client_id=<?php print GITHUB_CLIENT_ID ?>&scope=public_repo" onclick="trackOutboundLink(this, 'Clicks', 'github-login', 'landing', 'hero-button'); return false;">
+      <span>Log-in with GitHub</span>
+    </a>
+  </p>
+</div>
--- header.view.php
+++ header.view.php
@@ -1,8 +1,19 @@
-<html>
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-  <title>phpDoc generator</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <title>phpDoc in the cloud - generate phpDoc from GitHub repos</title>
+  <meta name="description" content="Generate and host your phpDoc code documentation on the cloud">
+  <meta name="viewport" content="width=device-width">
+
+  <link rel="stylesheet" href="/assets/css/bootstrap.css">
+  <link rel="stylesheet" href="/assets/css/main.css">
+  
   <style>
-  body { font-family: arial, sans-serif; }
   .header { overflow: hidden; }
   .notify { padding: 0 1em; }
   .notify.success { border: 1px solid #be7; background: #f8fff0; }
@@ -11,6 +22,18 @@
   .error { color: red; }
   </style>
   
+  <script type="text/javascript">
+  function trackOutboundLink(link, category, action, label, value) { 
+    try { 
+      _gaq.push(['_trackEvent', category , action, label, value]); 
+    } catch(err){}
+
+    setTimeout(function() {
+      document.location.href = link.href;
+    }, 100);
+  }
+  </script>
+
   <script type="text/javascript">
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', '<?php print GA_ID ?>']);
@@ -21,6 +44,6 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-</script>
+  </script>
 </head>
 <body>
